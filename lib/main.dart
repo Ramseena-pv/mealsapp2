5bc6200344b63772091d3c2e345e3b7cd3ea29a3
Ramseena-PV <ramseenapv123@gmail.com>
2024-03-22 22:40:58 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mealsapp2/data/dummy_data.dart';
-import 'package:mealsapp2/screens/categories.dart';
-import 'package:mealsapp2/screens/meals.dart';
-//import 'package:mealsapp2/widgets/category_grid_item.dart';
+
+import 'package:mealsapp2/screens/tabs.dart';
+
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -26,8 +26,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
-       
+      home: const TabsScreen(),
     );
   }
 }
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mealsapp2/screens/categories.dart';
 
 
 class Category {
